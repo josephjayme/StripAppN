00c1a05664b06f9e7ee9c68505361132ba460682
josephjayme <jayme.josef@gmail.com>
2017-12-19 23:04:28 +0800

    edited app.py for heroku

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def add_cors(resp):
 
 if __name__ == '__main__':
     app.debug=True
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
